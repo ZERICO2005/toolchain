4132c3048e8688206116fa1492b181e851786b67
unknown <71151164+ZERICO2005@users.noreply.github.com>
2025-01-17 12:52:10 -0700

    Added assembly routines for floating point classification

--- src/libc/isnormal.c
+++ src/libc/isnormal.c
@@ -1,24 +1,4 @@
 #include <stdint.h>
-#include <math.h>
-
-typedef union F32_pun {
-    float flt;
-    uint32_t bin;
-} F32_pun;
-
-#define Float32_exp_mask INT32_C(0x7F800000)
-
-int _isnormalf(float x)
-{
-    F32_pun val;
-    val.flt = x;
-    val.bin &= Float32_exp_mask;
-    
-    // Check that the exponent isn't all zeros (subnormal) or all ones (nan/inf)
-    return (val.bin != 0 && val.bin != Float32_exp_mask);
-}
-
-int _isnormal(double) __attribute__((alias("_isnormalf")));
 
 typedef union F64_pun {
     long double flt;
--- src/libc/issubnormal.c
+++ src/libc/issubnormal.c
@@ -2,27 +2,6 @@
 #include <stdint.h>
 #include <math.h>
 
-typedef union F32_pun {
-    float flt;
-    uint32_t bin;
-} F32_pun;
-
-#define Float32_exp_mask UINT32_C(0x7F800000)
-
-int _issubnormalf(float x) {
-    // zero is not a subnormal number
-    if (iszero(x)) {
-        return false;
-    }
-    F32_pun val;
-    val.flt = x;
-    val.bin &= Float32_exp_mask;
-    // check if the exponent is all zeros
-    return (val.bin == 0);
-}
-
-int _issubnormal(double) __attribute__((alias("_issubnormalf")));
-
 typedef union F64_pun {
     long double flt;
     uint64_t bin;
