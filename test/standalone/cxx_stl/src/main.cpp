1cff62ac27e73d0d61b948bb800223b3abcd05d8
unknown <71151164+ZERICO2005@users.noreply.github.com>
2025-09-02 17:04:50 -0600

    re-added our <type_traits> implementation and a few other bits of code

--- test/standalone/cxx_stl/src/main.cpp
+++ test/standalone/cxx_stl/src/main.cpp
@@ -1,6 +1,7 @@
 #include <ti/screen.h>
 #include <ti/getcsc.h>
 #include <sys/util.h>
+#include <ti/sprintf.h>
 #include <cstdio>
 
 int test();
@@ -9,9 +10,11 @@ int main(void) {
     os_ClrHome();
     int failed_test = test();
     if (failed_test != 0) {
-        std::printf("Failed test L%d\n", failed_test);
+        char buf[sizeof("Failed test L-8388608\n")];
+        boot_sprintf(buf, "Failed test L%d\n", failed_test);
+        std::fputs(buf, stdout);
     } else {
-        std::printf("All tests passed");
+        std::fputs("All tests passed", stdout);
     }
 
     while (!os_GetCSC());
