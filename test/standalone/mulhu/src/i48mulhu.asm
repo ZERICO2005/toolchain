d73aefbe02c9248264efebf434737a356c76a9b8
unknown <71151164+ZERICO2005@users.noreply.github.com>
2025-09-28 21:47:54 -0600

    close with i48mulhu

--- test/standalone/mulhu/src/i48mulhu.asm
+++ test/standalone/mulhu/src/i48mulhu.asm
@@ -13,23 +13,22 @@ __i48mulhu:
 	add	ix, sp
 	push	de
 	push	hl
-	push	af, af, af, af
+	push	af
 	ld	iy, 0
 
 	; x_lo * y_lo
 	lea	de, iy
-	ld	bc, (ix + 0)
-	ld	hl, (ix - 6)
 	call	__i48mulu
 	ld	(ix - 9), de	; (uint24_t)(UDE:UHL >> 24)
 
 	; x_hi * y_lo
 	lea	de, iy
-	ld	bc, (ix + 0)
 	ld	hl, (ix - 3)
 	call	__i48mulu
-	ld	(ix - 15), hl
-	ld	(ix - 12), de
+	; ld	(ix - 12), de
+	; ld	(ix - 15), hl
+	push	de		; hi24
+	push	hl		; lo24
 
 	; x_lo * y_hi
 	lea	de, iy
@@ -44,19 +43,21 @@ __i48mulhu:
 	jr	nc, .no_carry_1
 	inc	de
 .no_carry_1:
-	ld	bc, (ix - 15)
+	; ld	bc, (ix - 15)
+	pop	bc		; lo24
 	add	hl, bc
 	ex	de, hl
-	ld	bc, (ix - 12)
+	pop	bc		; hi24
+	; ld	bc, (ix - 12)
 	adc	hl, bc
-	ld	(ix - 18), hl
+	push	hl		; low order terms
 
 	; x_hi * y_hi
 	lea	de, iy
 	ld	bc, (ix + 3)
 	ld	hl, (ix - 3)
 	call	__i48mulu
-	ld	bc, (ix - 18)
+	pop	bc		; low order terms
 	add	hl, bc
 	jr	nc, .no_carry_2
 	inc	de
