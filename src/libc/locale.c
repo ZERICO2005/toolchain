0be937d2ffbbd9608c729f43b6d87b06224dd6a8
zerico <71151164+ZERICO2005@users.noreply.github.com>
2025-11-01 23:31:37 -0600

    added locale.h and added more wchar prototypes

---
+++ src/libc/locale.c
@@ -0,0 +1,37 @@
+#include <limits.h>
+#include <locale.h>
+
+struct lconv __c_locale = {
+    .decimal_point =       ".",
+    .thousands_sep =       "",
+    .grouping =            "",
+    .mon_decimal_point =   "",
+    .mon_thousands_sep =   "",
+    .mon_grouping =        "",
+    .positive_sign =       "",
+    .negative_sign =       "",
+    .currency_symbol =     "",
+    .frac_digits =         CHAR_MAX,
+    .p_cs_precedes =       CHAR_MAX,
+    .n_cs_precedes =       CHAR_MAX,
+    .p_sep_by_space =      CHAR_MAX,
+    .n_sep_by_space =      CHAR_MAX,
+    .p_sign_posn =         CHAR_MAX,
+    .n_sign_posn =         CHAR_MAX,
+    .int_curr_symbol =     "",
+    .int_frac_digits =     CHAR_MAX,
+    .int_p_cs_precedes =   CHAR_MAX,
+    .int_n_cs_precedes =   CHAR_MAX,
+    .int_p_sep_by_space =  CHAR_MAX,
+    .int_n_sep_by_space =  CHAR_MAX,
+    .int_p_sign_posn =     CHAR_MAX,
+    .int_n_sign_posn =     CHAR_MAX,
+};
+
+char* setlocale(__attribute__((__unused__)) int category, __attribute__((__unused__)) const char* locale) {
+    return "C";
+}
+
+struct lconv* localeconv(void) {
+    return &__c_locale;
+}
