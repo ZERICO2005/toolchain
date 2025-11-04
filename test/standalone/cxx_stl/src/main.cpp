dd7991f994546367a8be2309b08cfad1b349c527
zerico <71151164+ZERICO2005@users.noreply.github.com>
2025-11-03 21:45:15 -0700

    WHY WONT YOU LINK/ASSEMBLE

--- test/standalone/cxx_stl/src/main.cpp
+++ test/standalone/cxx_stl/src/main.cpp
@@ -4,7 +4,36 @@
 #include <ti/sprintf.h>
 #include <cstdio>
 
+#include <iostream>
+
 int test();
+
+void func() {
+    using namespace std::literals;
+
+    // Creating a string from const char*
+    std::string str1 = "hello";
+
+    // Creating a string using string literal
+    auto str2 = "world"s;
+
+    // Concatenating strings
+    std::string str3 = str1 + " " + str2;
+
+    // Print out the result
+    std::cout << str3 << '\n';
+
+    std::string::size_type pos = str3.find(" ");
+    str1 = str3.substr(pos + 1); // the part after the space
+    str2 = str3.substr(0, pos);  // the part till the space
+
+    std::cout << str1 << ' ' << str2 << '\n';
+
+    // Accessing an element using subscript operator[]
+    std::cout << str1[0] << '\n';
+    str1[0] = 'W';
+    std::cout << str1 << '\n';
+}
 
 int main(void) {
     os_ClrHome();
@@ -16,6 +45,8 @@ int main(void) {
     } else {
         std::fputs("All tests passed", stdout);
     }
+
+    func();
 
     while (!os_GetCSC());
 
